73297fd3e51333ea88a0458515a42e999d6daf7a
AitorMT <25897764+AitorMT@users.noreply.github.com>
2025-07-07 16:35:53 +0200

    fonts and images

---
+++ src/consts/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts = {
+    Sora: ["/fonts/Sora/Sora-Regular.woff2", "/fonts/Sora/Sora-Bold.woff2"],
+    Inter: ["/fonts/Inter/Inter-Regular.woff2", "/fonts/Inter/Inter-Bold.woff2"]
+}
