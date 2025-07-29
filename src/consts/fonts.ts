7de44e893eb12908b8e04f3c150b45ca7c52fa3e
AitorMT <25897764+AitorMT@users.noreply.github.com>
2025-07-29 16:03:00 +0200

    Archivos iniciales Music Player
    Se han añadido los archivos iniciales para la página de Music Player

--- src/consts/fonts.ts
+++ src/consts/fonts.ts
@@ -3,4 +3,5 @@ export const fonts = {
   IndexPage: ['/fonts/Inter/Inter-Regular.woff2', '/fonts/Inter/Inter-Bold.woff2'],
   BusinessBlogCard: ['/fonts/Poppins/Poppins-Medium.woff2', '/fonts/Lato/Lato-Regular.woff2'],
   TestimonialPage: ['/fonts/Sora/Sora-Regular.woff2', '/fonts/Sora/Sora-Bold.woff2'],
+  MusicPlayer: ['/fonts/Inter/Inter-Regular.woff2'],
 }
