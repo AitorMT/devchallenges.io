139233c688b78e630fe16dfc841f19761e348ee3
AitorMT <25897764+AitorMT@users.noreply.github.com>
2025-07-31 10:24:47 +0200

    Añadida la dependencia @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,66 @@
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
+import Compress from '@playform/compress'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://devchallenges.aitormt.dev',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  build: {
+    inlineStylesheets: 'auto',
+  },
+
+  compressHTML: true,
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  integrations: [
+    Compress({
+      CSS: {
+        lightningcss: {
+          minify: true,
+        },
+      },
+      HTML: {
+        'html-minifier-terser': {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+          removeAttributeQuotes: true,
+          minifyCSS: true,
+          minifyJS: true,
+        },
+      },
+      Image: {
+        webp: {
+          quality: 75,
+        },
+        mozjpeg: { quality: 75 },
+        optipng: { optimizationLevel: 5 },
+        svgo: true,
+      },
+      JavaScript: {
+        terser: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+      },
+      JSON: true,
+      SVG: true,
+    }),
+  ],
 })
