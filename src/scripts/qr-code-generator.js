4d1dd4da09ab99e8765f3845b261d221bb397008
AitorMT <25897764+AitorMT@users.noreply.github.com>
2025-08-03 21:25:06 +0200

    Diseño QR Code Generator Finalizado
    El diseño y la lógica estan terminados, podría mejorarse considerablemente con más controles

---
+++ src/scripts/qr-code-generator.js
@@ -0,0 +1,71 @@
+const onClickGenerateQR = (text, id) => {
+  const container = document.getElementById(id)
+  if (!container) return console.error(`No se encontró el elemento con id: ${id}`)
+  container.innerHTML = ''
+  new QRCode(container, {
+    text,
+    width: 184,
+    height: 184,
+    render: 'image',
+    colorDark: '#111629',
+    correctLevel: QRCode.CorrectLevel.H,
+  })
+  document.getElementById('input-container').classList.add('hidden')
+  document.getElementById('qr-code-container').classList.remove('hidden')
+}
+
+const onClickDownloadBtn = (downloadBtnId, qrContainerId) => {
+  const btn = document.getElementById(downloadBtnId)
+  if (!btn) return
+
+  btn.addEventListener('click', () => {
+    const container = document.getElementById(qrContainerId)
+    if (!container) return
+
+    const img = container.querySelector('img')
+
+    const link = document.createElement('a')
+    link.href = img.src
+    link.download = 'qr-code.png'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  })
+}
+
+const onClickShareButton = (shareBtnId, qrContainerId) => {
+  const btn = document.getElementById(shareBtnId)
+  if (!btn) return
+
+  btn.addEventListener('click', async () => {
+    const container = document.getElementById(qrContainerId)
+    if (!container) return
+
+    const img = container.querySelector('img')
+
+    try {
+      const response = await fetch(img.src)
+      const blob = await response.blob()
+
+      await navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })])
+    } catch (err) {
+      console.error(err.message)
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('generate-qr-btn')
+  const input = document.getElementById('qr-input')
+  if (!button || !input) return
+  button.addEventListener('click', () => {
+    if (!input.value) {
+      alert('Please enter a value to generate the QR code.')
+      return
+    } else {
+      onClickGenerateQR(input.value, 'qr-code')
+    }
+  })
+  onClickDownloadBtn('download-qr-btn', 'qr-code')
+  onClickShareButton('share-qr-btn', 'qr-code')
+})
